421bacc70407a7805f8fed04808ccf10c8a72718
Kaen Pawanachai <pawaox4th@gmail.com>
2023-08-01 15:53:26 +0700

    feat: add 4

--- src/basic/3-Null-vs-Undefined.ts
+++ src/basic/3-Null-vs-Undefined.ts
@@ -1,5 +1,21 @@
 // NOTE: Disable "strict" rule in tsconfig.json to avoid error.
 
-let message = "hello"
+let message = "1hello"
+
 // // message = null
 console.log("🐳 MESSAGE[0].TOUPPERCASE() :", message[0].toUpperCase())
+
+/**
+ *
+ * EX. upperCaseFirstLetter()
+ */
+function upperCaseFirstLetter(str: string | null) {
+  // Solve 1
+  // return str?.[0].toUpperCase() + str?.substr(1)
+
+  // Solve 2
+  if (str && typeof str === "string") {
+    return str[0].toUpperCase() + str.substr(1)
+  }
+  return str
+}
---
+++ src/basic/4-any-unknow.ts
@@ -0,0 +1,9 @@
+// NOTE: Disable "strict" rule in tsconfig.json to avoid error.
+
+// NOTE: "any" type is the most flexible type in TypeScript.
+
+function onFixed(num: number) {
+  return num.toFixed(2)
+}
+
+let num = 1234.111
