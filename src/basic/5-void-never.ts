b41417feb8284be36aa39eecfeeb836b5aa8cabe
Kaen Pawanachai <pawaox4th@gmail.com>
2023-08-01 16:11:11 +0700

    feat: create unit 4 and 5 create unit 4 and 5

---
+++ src/basic/5-void-never.ts
@@ -0,0 +1,10 @@
+// function formatPrice(priceValue: string | number) {
+//   switch (typeof priceValue) {
+//     case "number":
+//       return priceValue.toFixed(2)
+//     case "string":
+//       return parseFloat(priceValue).toFixed(2)
+//     default:
+//       return priceValue
+//   }
+// }
