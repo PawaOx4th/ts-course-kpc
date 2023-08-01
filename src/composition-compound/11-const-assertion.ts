97a1915b04a1fa0b8dbd241bc0a1dba61cef4653
Kaen Pawanachai <pawaox4th@gmail.com>
2023-08-01 22:22:17 +0700

    feat: add unit const assertion

---
+++ src/composition-compound/11-const-assertion.ts
@@ -0,0 +1,22 @@
+/**
+ * Unit : const Assertions.
+ */
+// NOTE: const Assertions.
+// const Assertions are a way to tell the compiler that the type of some entity
+// is more specific than its current type.
+// ------------------------------------------------------------
+//  - const Assertions are used to narrow down the type of an
+//    - variable.
+//    - parameter.
+//    - property.
+//    - function.
+//    - class.
+//    - generic type.
+
+// Example 1: const Assertions with `as const`.
+
+// // person.name = "Jane"
+// // Error: Cannot assign to 'name' because it is a read-only property.
+
+// // person.address.city = "Mysore"
+// // Error: Cannot assign to 'city' because it is a read-only property.
